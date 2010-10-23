56273c725980f9f28d78491830541f9c20af06b2
Vitor Nazário <vncoelho@gmail.com>
2010-10-23 14:30:44 +0000

    The class Timer.hpp now shows the wall time.

--- OptFrame/Util/Timer.hpp
+++ OptFrame/Util/Timer.hpp
@@ -4,18 +4,30 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <sys/time.h>
+#include <sys/timeb.h>
+#include <sys/resource.h>
 
 class Timer 
 {
 protected:
-	clock_t initialTime;
-	bool showMessageOnDestroy;
-public:
-	Timer(){ initialTime=clock(); showMessageOnDestroy=true; };
-	Timer(bool m){ initialTime=clock(); showMessageOnDestroy=m; };
+	double initialTime;
 
-	virtual ~Timer(){ if(showMessageOnDestroy) printf("Spent time: %.3f secs\n", (double) (clock() - initialTime) / CLOCKS_PER_SEC ); };
-	virtual double now() { return ((double)(clock() - initialTime) / CLOCKS_PER_SEC) ; };
+	bool showMessageOnDestroy;
+
+	double get_clock_sec(void)
+	{  struct timeval t;
+		struct timezone tz;
+		gettimeofday(&t, &tz);
+		return (double) t.tv_sec + (double) t.tv_usec * 1E-6;
+	};
+
+public:
+	Timer(){ initialTime=get_clock_sec(); showMessageOnDestroy=true; };
+	Timer(bool m){ initialTime=get_clock_sec(); showMessageOnDestroy=m; };
+	virtual ~Timer(){ if(showMessageOnDestroy) printf("Spent time: %f secs\n", get_clock_sec() -initialTime) ; };
+	virtual double now() { return (get_clock_sec() - initialTime) ;  };
+
 };
 
 #endif /* TIMER_HPP */
