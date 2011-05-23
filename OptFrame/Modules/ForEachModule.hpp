5e9e436a3ae2ff439d7e6786a5fc396ca222a424
Pablo Munhoz <pablo.munhoz@gmail.com>
2011-05-23 05:57:08 +0000

    Including $ on variables of ForEach module.

--- OptFrame/Modules/ForEachModule.hpp
+++ OptFrame/Modules/ForEachModule.hpp
@@ -21,6 +21,8 @@
 #ifndef FOREACHMODULE_HPP_
 #define FOREACHMODULE_HPP_
 
+#include<string>
+
 #include "../OptFrameModule.hpp"
 
 template<class R, class M>
@@ -35,7 +37,135 @@ private:
 		return NULL;
 	}
 
-	bool exec_modules(vector<OptFrameModule<R, M>*> all_modules, HeuristicFactory<R, M>* factory, map<string, string>* dictionary, string command)
+	string var_preprocess(string var, string value, string command)
+	{
+		string new_command = "";
+		string rest = "";
+		int dollar_pos = -1;
+
+		for (int i = 0; i < command.length(); i++)
+		{
+			if (command[i] == '$')
+			{
+				dollar_pos = i;//first dollar position
+				break;
+			}
+		}
+
+		if (dollar_pos == -1) //dollar not found
+			new_command = command;
+		else
+		{
+			if (command[dollar_pos + 1] == '{') //var with brackets
+			{
+				//verifying if have close bracket for this var
+				if (command[dollar_pos + var.length() + 1] == '}')
+				{
+					//verifying if the found var is the same parameter var
+					bool test_var = true;
+					for (int i = 1; i < var.length(); i++)//ignoring the $ in the var (i = 1)
+					{
+						if (var[i] != command[dollar_pos + 1 + i])
+						{
+							test_var = false;
+							break;
+						}
+					}
+					if (test_var) // correctly variable found
+					{
+						//replacing variable
+						new_command.append(command, 0, dollar_pos); //first part of command without bracket
+						new_command.append(value); // value of variable
+						rest.append(command.begin() + dollar_pos + var.length() + 2, command.end());//rest of command
+						//verifying if have the same var in the rest of command
+						rest = var_preprocess(var, value, rest);
+
+						new_command.append(rest);
+					}
+					else//can be another variable
+					{
+						//dont replace the variable
+						new_command.append(command, 0, dollar_pos + var.length() + 2);
+
+						rest.append(command.begin() + dollar_pos + var.length() + 2, command.end());//rest of command
+
+						//verifying if have the var in the rest of command
+						rest = var_preprocess(var, value, rest);
+
+						new_command.append(rest);
+
+					}
+
+				}
+				else//can be another variable
+				{
+					//ignoring the variable found and continue finding for the parameter variable in the rest of command
+					new_command.append(command, 0, dollar_pos + 2);
+					rest.append(command.begin() + dollar_pos + 2, command.end());
+
+					rest = var_preprocess(var, value, rest);
+
+					new_command.append(rest);
+
+				}
+			}
+			else// variable without brackets
+			{
+				if (isalnum(command[dollar_pos + var.length()]) || (command[dollar_pos + var.length()] == '_'))// don't have the same length, can be another variable
+				{
+					new_command.append(command, 0, dollar_pos + 1);
+					//finding for the parameter variable in the rest of command
+					rest.append(command.begin() + dollar_pos + 1, command.end());
+
+					rest = var_preprocess(var, value, rest);
+
+					new_command.append(rest);
+				}
+				else // have the same length
+				{
+					//verifying if the found var is the same parameter var
+					bool test_var = true;
+					for (int i = 1; i < var.length(); i++)//ignoring the $ in the var (i = 1)
+					{
+						if (var[i] != command[dollar_pos + i])
+						{
+							test_var = false;
+							break;
+						}
+					}
+					if (test_var) // correctly variable found
+					{
+						//replacing variable
+						new_command.append(command, 0, dollar_pos); //first part of command without dollar
+						new_command.append(value); // value of variable
+						rest.append(command.begin() + dollar_pos + var.length(), command.end());//rest of command
+						//verifying if have the same var in the rest of command
+						rest = var_preprocess(var, value, rest);
+
+						new_command.append(rest);
+					}
+					else//can be another variable
+					{
+						//dont replace the variable
+						new_command.append(command, 0, dollar_pos + var.length());
+
+						rest.append(command.begin() + dollar_pos + var.length(), command.end());//rest of command
+
+						//verifying if have the var in the rest of command
+						rest = var_preprocess(var, value, rest);
+
+						new_command.append(rest);
+					}
+
+				}
+			}
+		}
+
+		return new_command;
+
+	}
+
+	bool exec_command(vector<OptFrameModule<R, M>*> all_modules, HeuristicFactory<R, M>* factory, map<string, string>* dictionary, string command)
 	{
 		Scanner scanner(command);
 		string module = scanner.next();
@@ -57,7 +187,7 @@ public:
 	}
 	string usage()
 	{
-		return "for_each var list_of_values list_of_commands";
+		return "for_each $var list_of_values list_of_commands";
 	}
 	void run(vector<OptFrameModule<R, M>*> all_modules, HeuristicFactory<R, M>* factory, map<string, string>* dictionary, string input)
 	{
@@ -70,6 +200,13 @@ public:
 		}
 
 		string var = scanner.next();
+
+		if (var[0] != '$')
+		{
+			cout << "Missing operator $ in variable: " << var << endl;
+			return;
+		}
+
 		vector < string > values;
 		vector < string > commands;
 
@@ -78,11 +215,10 @@ public:
 
 		for (int v = 0; v < values.size(); v++)
 		{
-			(*dictionary)[var] = values.at(v);
-
 			for (int c = 0; c < commands.size(); c++)
 			{
-				if (!exec_modules(all_modules, factory, dictionary, commands.at(c)))
+				string command = var_preprocess(var, values.at(v), commands.at(c));
+				if (!exec_command(all_modules, factory, dictionary, command))
 				{
 					cout << "Error in command: " << commands.at(c) << endl;
 				}
