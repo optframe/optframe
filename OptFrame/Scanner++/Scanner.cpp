4d163629450844a8e95a203835937c81e8b360f0
Igor Machado <igor.machado@gmail.com>
2012-03-07 19:17:53 +0000

    Branch and tag pointed to Scanner++ repository and didn't have the real files.

---
+++ OptFrame/Scanner++/Scanner.cpp
@@ -0,0 +1,368 @@
+// OptFrame - Optimization Framework
+
+// Copyright (C) 2009, 2010, 2011
+// http://optframe.sourceforge.net/
+//
+// This file is part of the OptFrame optimization framework. This framework
+// is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License v3 as published by the
+// Free Software Foundation.
+
+// This framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License v3 for more details.
+
+// You should have received a copy of the GNU Lesser General Public License v3
+// along with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// Scanner Object - Scanner++
+
+// Copyright (C) 2009, 2010, 2011
+// Igor Machado - Mario Henrique Perche - Pablo Munhoz - Sabir Ribas
+//
+// This file is part of the Scanner++ Library v0.93.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License v3 as published by the
+// Free Software Foundation.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License v3 for more details.
+
+// You should have received a copy of the GNU Lesser General Public License v3
+// along with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#include "Scanner.h"
+
+Scanner::Scanner(File* inputfile)
+{
+	this->inputfile = inputfile;
+	this->input = inputfile->file;
+	useDefaultSeparators();
+}
+
+Scanner::Scanner(istream* input)
+{
+	this->inputfile = NULL;
+	this->input = input;
+	useDefaultSeparators();
+}
+
+Scanner::Scanner(string input)
+{
+	this->inputfile = NULL;
+	this->input = new istringstream(input);
+	useDefaultSeparators();
+}
+
+Scanner::~Scanner()
+{
+	close();
+}
+
+void Scanner::useDefaultSeparators()
+{
+	useSeparators(string("\n\r\t "));
+}
+
+void Scanner::useSeparators(string s)
+{
+	sep = s;
+}
+
+bool Scanner::inSeparators(char c)
+{
+	for (unsigned int i = 0; i < sep.length(); i++)
+		if (sep[i] == c)
+			return true;
+	return false;
+}
+
+// =================================================================
+// =================================================================
+
+bool Scanner::hasNextChar()
+{
+	if (input->eof())
+		return false;
+
+	int x = input->get();
+
+	if (x > 0)
+	{
+		input->putback((char) x);
+		return true;
+	}
+
+	if (x == 0)
+	{
+		input->putback((char) x);
+		return false;
+	}
+
+	return false;
+}
+
+char Scanner::nextChar()
+{
+	int x = input->get();
+
+	if (x <= 0)
+	{
+		cerr << "Excecao: Nao ha proximo char!" << endl;
+		exit(1);
+	}
+
+	return x;
+}
+
+// =================================================================
+// =================================================================
+
+int Scanner::nextInt()
+{
+	int x;
+	istringstream myStream(next());
+	if (myStream >> x)
+		return x;
+	else
+	{
+		cerr << "Scanner++: Falha na conversao nextInt()." << endl;
+		exit(1);
+	}
+}
+
+long Scanner::nextLong()
+{
+	long x;
+	istringstream myStream(next());
+	if (myStream >> x)
+		return x;
+	else
+	{
+		cerr << "Scanner++: Falha na conversao nextLong()." << endl;
+		exit(1);
+	}
+}
+
+float Scanner::nextFloat()
+{
+	float x;
+	istringstream myStream(next());
+	if (myStream >> x)
+		return x;
+	else
+	{
+		cerr << "Scanner++: Falha na conversao nextFloat()." << endl;
+		exit(1);
+	}
+
+}
+
+double Scanner::nextDouble()
+{
+	double x;
+	istringstream myStream(next());
+	if (myStream >> x)
+		return x;
+	else
+	{
+		cerr << "Scanner++: Falha na conversao nextDouble()." << endl;
+		exit(1);
+	}
+
+}
+
+// =================================================================
+// =================================================================
+
+bool Scanner::hasNext()
+{
+	if (!hasNextChar())
+		return false;
+
+	vector<char> buffer;
+
+	char novo = nextChar();
+
+	bool next = true;
+
+	while (inSeparators(novo))
+	{
+		buffer.push_back(novo);
+
+		if (!(hasNextChar()))
+		{
+			next = false;
+			break;
+		}
+
+		novo = nextChar();
+	}
+
+	if (next) // ha proximo!
+		input->putback(novo);
+
+	//devolver o buffer ao IO
+	while (buffer.size() > 0)
+	{
+		input->putback(buffer.at(buffer.size() - 1));
+		buffer.erase(buffer.begin() + (buffer.size() - 1));
+	}
+
+	return next;
+}
+
+std::string Scanner::next()
+{
+	discarded = "";
+
+	std::string x = "";
+
+	while (hasNextChar())
+	{
+		char c = nextChar();
+
+		if (!inSeparators(c))
+		{
+			x = x + c;
+			break;
+		}
+		else
+			discarded += c;
+	}
+
+	while (hasNextChar())
+	{
+		char c = nextChar();
+
+		if (inSeparators(c))
+		{
+			input->putback(c);
+			break;
+		}
+
+		x = x + c;
+	}
+
+	return x;
+}
+
+pair<string, map<string, string> > Scanner::nextXMLTag()
+{
+	string x = "";
+
+	while (hasNextChar())
+	{
+		char c = nextChar();
+
+		if (c == '<')
+		{
+			x = x + c;
+			break;
+		}
+	}
+
+	while (hasNextChar())
+	{
+		char c = nextChar();
+		x = x + c;
+
+		if (c == '>')
+			break;
+	}
+
+	cout << "base: " << x << endl;
+
+	if (x.size() < 2 || x.at(0) != '<' || x.at(x.size() - 1) != '>')
+		return make_pair("", map<string, string> ());
+
+	Scanner scanner(x);
+	scanner.useSeparators("<>");
+
+	string tagname = "";
+	map<string, string> attr;
+
+	if (scanner.hasNext())
+	{
+		string tag = scanner.next();
+		//cout << "tag: " << tag << endl;
+
+		Scanner sc_tag(tag);
+		sc_tag.useSeparators(" ");
+
+		if (sc_tag.hasNext())
+			tagname = sc_tag.next();
+
+		//cout << "tagname: " << tagname << endl;
+
+		// TODO usar trim
+
+		sc_tag.useSeparators(" =");
+		while (sc_tag.hasNext())
+		{
+			string at_name = sc_tag.next();
+			//cout << "at_name: " << at_name << "\t";
+			sc_tag.useSeparators("=\"");
+
+			string at_value = sc_tag.next();
+			//cout << "at_value: " << at_value << "\t";
+
+			attr[at_name] = at_value;
+
+			sc_tag.useSeparators("\" =");
+		}
+	}
+
+	return make_pair(tagname, attr);
+}
+
+bool Scanner::hasNextLine()
+{
+	//cout << "Unimplemented hasNextLine() -> calling hasNext()" << endl;
+	return hasNext();
+}
+
+std::string Scanner::nextLine()
+{
+	string backup_sep = sep;
+	useSeparators("\n");
+	string linha = next();
+	useSeparators(backup_sep);
+
+	return linha;
+}
+
+// =================================================================
+// =================================================================
+
+void Scanner::close()
+{
+	if (inputfile)
+	{
+		delete inputfile;
+		inputfile = NULL;
+	}
+}
+
+string Scanner::rest() // Returns the rest of the input as string
+{
+	string backup_sep = sep;
+
+	useSeparators("");
+
+	string rest = "";
+
+	if (hasNext())
+		rest = next();
+
+	sep = backup_sep;
+
+	return rest;
+}
---
+++ OptFrame/Scanner++/Scanner.h
@@ -0,0 +1,175 @@
+// OptFrame - Optimization Framework
+
+// Copyright (C) 2009, 2010, 2011
+// http://optframe.sourceforge.net/
+//
+// This file is part of the OptFrame optimization framework. This framework
+// is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License v3 as published by the
+// Free Software Foundation.
+
+// This framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License v3 for more details.
+
+// You should have received a copy of the GNU Lesser General Public License v3
+// along with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+// Scanner Object - Scanner++
+
+// Copyright (C) 2009, 2010, 2011
+// Igor Machado - Mario Henrique Perche - Pablo Munhoz - Sabir Ribas
+//
+// This file is part of the Scanner++ Library v0.93.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License v3 as published by the
+// Free Software Foundation.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License v3 for more details.
+
+// You should have received a copy of the GNU Lesser General Public License v3
+// along with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+
+#ifndef SCANNERPP_H_
+#define SCANNERPP_H_
+
+#include<iostream>
+#include<istream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+
+#include<vector>
+#include<map>
+
+#include <stdlib.h>
+
+using namespace std;
+
+#include "File.h"
+
+class Scanner
+{
+private:
+	istream *input;
+	File* inputfile;
+	string sep;
+
+	string discarded;
+
+public:
+
+	string getDiscarded()
+	{
+		return discarded;
+	}
+
+	bool hasNextChar();
+	char nextChar();
+
+	Scanner(File* inputfile);
+	Scanner(istream* input);
+	Scanner(string input);
+
+	virtual ~Scanner();
+
+	// useDefaultSeparators: chama o useSeparators para os caracteres:
+	// espaco, quebra de linha (\n), tabulacao (\t) e retorno de carro (\r)
+
+	void useDefaultSeparators();
+
+	// useSeparators: equivalente ao useDelimiter de Java
+	// a diferenca e que Java trata a string como uma
+	// expressao regular, e neste caso o useSeparators
+	// apenas considera cada caractere da string separadamente
+	// como um separador.
+
+	void useSeparators(string s);
+	bool inSeparators(char c);
+
+	std::string next();
+	std::string nextLine();
+	int nextInt();
+	long nextLong();
+	float nextFloat();
+	double nextDouble();
+	//static string trim(string);
+	static string trim(string word)
+	{
+		int i = 0;
+		char c = word[i];
+		string aux_word = "";
+
+		//removing initial spaces
+		while (c == ' ' && i < ((int)word.length()))
+		{
+			i++;
+			c = word[i];
+		}
+
+		while (i < ((int)word.length()))
+		{
+			aux_word += word[i];
+			i++;
+		}
+
+		word = aux_word;
+		//removing final spaces
+		i = word.length() - 1;
+		c = word[i];
+		aux_word = "";
+		while (c == ' ' && i >= 0)
+		{
+			i--;
+			c = word[i];
+		}
+
+		while (i >= 0)
+		{
+			aux_word += word[i];
+			i--;
+		}
+
+		word = aux_word;
+
+		//reverting the word
+		reverse(word.begin(), word.end());
+
+		return word;
+	}
+
+	pair<string, map<string, string> > nextXMLTag();
+
+	bool hasNext();
+	bool hasNextLine(); // Unimplemented! Calling hasNext() instead
+	bool hasNextInt()
+	{
+		return true;
+	}
+	bool hasNextLong()
+	{
+		return true;
+	}
+	bool hasNextFloat()
+	{
+		return true;
+	}
+	bool hasNextDouble()
+	{
+		return true;
+	}
+
+	void close();
+
+	string rest(); // Returns the rest of the input as string
+};
+
+#endif /*SCANNERPP_H_*/
