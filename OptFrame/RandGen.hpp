22fbb2c2a2764a1a92e6226fcee222954cbb8c92
Vitor <vncoelho@gmail.com>
2017-04-17 23:31:38 -0300

    Method for generating random seed

--- OptFrame/RandGen.hpp
+++ OptFrame/RandGen.hpp
@@ -24,6 +24,7 @@
 #include <math.h>
 #include <time.h>
 #include <cmath>
+#include <unistd.h>
 //#include <tgmath.h>
 
 #include "Component.hpp"
@@ -209,6 +210,25 @@ public:
 	{
 		seed = _seed;
 		initialize();
+	}
+
+	unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
+	{
+		    a=a-b;  a=a-c;  a=a^(c >> 13);
+		    b=b-c;  b=b-a;  b=b^(a << 8);
+		    c=c-a;  c=c-b;  c=c^(b >> 13);
+		    a=a-b;  a=a-c;  a=a^(c >> 12);
+		    b=b-c;  b=b-a;  b=b^(a << 16);
+		    c=c-a;  c=c-b;  c=c^(b >> 5);
+		    a=a-b;  a=a-c;  a=a^(c >> 3);
+		    b=b-c;  b=b-a;  b=b^(a << 10);
+		    c=c-a;  c=c-b;  c=c^(b >> 15);
+		    return c;
+	}
+
+	unsigned long generateRandomSeed()
+	{
+		   return mix(clock(), time(NULL), getpid());
 	}
 
 	template<class T>
