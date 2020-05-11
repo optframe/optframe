a93ac4e5b848c522702fd4fa87d2b8697c415e5d
Igor Machado <igor.machado@gmail.com>
2020-05-11 18:29:53 -0300

    ready to build first example on FCore

--- tests/FCore-tests/FMove.Test.cpp
+++ tests/FCore-tests/FMove.Test.cpp
@@ -35,26 +35,26 @@ fooFunc(int n)
 
 auto barFunc = [](int n) -> bool { return true; };
 //
-template<class X>
+template<void (*X)(int)>
 class S
 {
 };
 
 // TODO: funciona o Y?
 //using Y = S< [](int x) -> void { std::cout << x << " hello\n"; } >;
-//using Y = S<+[](int x) -> void { std::cout << x << " hello\n"; }>;
+using Y = S<+[](int x) -> void { std::cout << x << " hello\n"; }>;
+
+//
+//
+using MyXSolution = Solution<std::vector<int>>;
+using MyXESolution = std::pair<MyXSolution, Evaluation<int>>;
+//
 
 using myDS = std::pair<int, int>;
-using MyXSolution = std::vector<int>;
-using MyXEvaluation = Evaluation<int>;
-using MyXESolution = std::pair<MyXSolution, MyXEvaluation>;
-
 // this needs to be in global scope... too bad!
 auto fCanBeApplied = [](const myDS&, const MyXESolution&) -> bool {
    return false;
 };
-
-//op<myDS> (*fApply)(MyXESolution&)
 
 auto fApply =
   [](const myDS& myData, MyXESolution& xes) -> op<myDS> {
@@ -62,13 +62,14 @@ auto fApply =
    return make_optional(p);
 };
 
-//extern bool (*fCanBeApplied2)(const MyXESolution&) 
+// function requires "linkage", thus we define on global scope
 
-// function requires "linkage"
-auto fCanBeApplied2 
-= [](const MyXESolution&) -> bool {
-      return false;
-   };
+using MyMoveSwap = FMove<
+  std::pair<int, int>,
+  MyXESolution,
+  [](const myDS& myData, MyXESolution& xes) -> op<myDS> {
+     return op<std::pair<int, int>>{ make_pair(myData.second, myData.first) };
+  }>;
 
 TEST_CASE("OptFrame FCore FMove: Create FMove")
 {
@@ -78,17 +79,17 @@ TEST_CASE("OptFrame FCore FMove: Create FMove")
    //
    //
 
+   MyMoveSwap mswap(make_pair(3, 4));
+
    myDS p(2, 3);
    //std::function<bool (const MyXESolution&)> fApply;
 
    FMove<myDS, MyXESolution, fApply, fCanBeApplied> fmove{ p };
 
-   
    FMove<
      myDS,
      MyXESolution,
-     fApply
-     >
+     fApply>
      fmoveDefault{ p };
 
    //Matrix<int> m(2);
