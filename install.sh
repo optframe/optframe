f7332c7d58d7aa46d5f4091b2140b11389bdaafb
Igor Machado <igor.machado@gmail.com>
2020-05-13 13:41:12 -0300

    fixed install script for OptFCore

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "This script requires 'sudo' to install OptFrame"
 
 if [[ ! $(sudo echo 0) ]]; then exit; fi
 
+echo "Installing OptFrame (with OptFCore) into /usr/local/include and /usr/local/optframe"
+
 cp -r src/OptFrame /usr/local/include/
+cp -r src/OptFCore /usr/local/include/
 
 mkdir /usr/local/optframe
 
