85d5a68d1b5a48d39dc6773b0eefd7894b3876ad
fellipessanha <69586375+fellipessanha@users.noreply.github.com>
2020-12-15 14:14:24 -0300

    Update PSO.hpp
    Added random mutations from python mother code and switched up the orders

--- src/OptFrame/Heuristics/EvolutionaryAlgorithms/PSO.hpp
+++ src/OptFrame/Heuristics/EvolutionaryAlgorithms/PSO.hpp
@@ -38,157 +38,6 @@
 
 namespace optframe {
 
-/*
-template<XEvaluation XEv = Evaluation<>, optframe::comparability KeyType = double>
-class RandomKeysInitPop : public InitialPopulation<std::pair<std::vector<KeyType>, XEv>>
-{
-   using RSK = std::vector<KeyType>;
-   using XES = std::pair<std::vector<KeyType>, XEv>;
-
-private:
-   int rksz;
-   RandGen& rg;
-
-public:
-   RandomKeysInitPop(int size, RandGen& _rg)
-     : rksz(size)
-     , rg{ _rg }
-   {
-   }
-
-   virtual Population<XES> generatePopulation(unsigned populationSize, double timelimit) override
-   {
-      Population<XES> pop;
-
-      for (unsigned i = 0; i < populationSize; i++) {
-         random_keys* d = new random_keys(rksz);
-         for (int j = 0; j < rksz; j++)
-            d->at(j) = (rg.rand() % 100000) / 100000.0; // TODO: take precision from Template or from good RNG
-         //RSK* sol = new RSK(d);
-         //pop.push_back(sol);
-         pop.push_back(d); // TODO: pass by std::move() or unique_ptr
-
-         if (Component::debug)
-            (*Component::logdata) << "generatePopulation new: " << *d << std::endl;
-      }
-
-      return pop;
-   }
-
-   virtual bool setVerboseR() override
-   {
-      this->setVerbose();
-      return InitialPopulation<XES>::setVerboseR();
-   }
-};
-
-*/
-
-// begin PSO
-
-struct Bird {
-    vector<double> velocity;
-    vector<double> position;
-};
-
-template<
-  optframe::comparability KeyType = double,
-  XEvaluation XEv = Evaluation<>,
-  XESolution XES = std::pair<std::vector<KeyType>, XEv>,
-  XESolution XES2 = std::pair<Bird, Evaluation<>>,
-  XSearch<XES2> XSH2 = Population<XES2>>
-class PSO : public SingleObjSearch<XES, XES2, XSH2>
-  , public IPopulational<XES, XES, XES2>
-{
-   using S = typename XES::first_type;
-   //using XEv = typename XES::second_type;
-   using XSH = XES;
-
-protected:
-   Evaluator<S, XEv, XES>& evaluator; // Check to avoid memory leaks
-   //InitialPopulation<XES2>& initPop; // TODO: add if necessary
-   /*
-      RANDOM PSO
-    * pop_size -> Número de pájaros (o partículas)
-    * iter_max -> Número máximo de iteraciones de PSO
-    * cI -> Vector con los topes inferiores de los parámetros
-    * cS -> Vector con los topes superiores de los parámetros
-    * FUNCION -> Función que pretendemos minimizar
-    * 
-    * */
-   // population size
-   unsigned pop_size;
-   // number of generations (stop criteria)
-   unsigned iter_max;
-   // -> Vector con los topes inferiores de los parámetros
-   vector<double> cI;
-   // -> Vector con los topes superiores de los parámetros
-   vector<double> cS;
-   // number of variables to optimize
-   int nVars; 
-   // random number generator
-   RandGen& rg;
-
-public:
-   PSO(Evaluator<S, XEv, XES>& evaluator, unsigned pop_size, unsigned iter_max, vector<double> cI, vector<double> cS, RandGen& _rg)
-     : evaluator(evaluator)
-     , pop_size(pop_size)
-     , iter_max(iter_max)
-     , cI(cI)
-     , cS(cS)
-     , rg{ _rg }
-   {
-      assert(cI.size() == cS.size());
-      this->nVars = cI.size();
-   }
-
-   virtual ~PSO()
-   {
-      // nothing to delete... NICE!
-   }
-
-   SearchStatus search(const StopCriteria<XEv>& stopCriteria) override
-   
-// OptFrame - Optimization Framework
-
-// Copyright (C) 2009, 2010, 2011
-// http://optframe.sourceforge.net/
-//
-// This file is part of the OptFrame optimization framework. This framework
-// is free software; you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License v3 as published by the
-// Free Software Foundation.
-
-// This framework is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License v3 for more details.
-
-// You should have received a copy of the GNU Lesser General Public License v3
-// along with this library; see the file COPYING.  If not, write to the Free
-// Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
-// USA.
-
-#ifndef OPTFRAME_PSO_HPP_
-#define OPTFRAME_PSO_HPP_
-
-#include <algorithm>
-
-#include "../../IPopulational.hpp"
-#include "../../InitialPopulation.h"
-#include "../../Population.hpp"
-#include "../../SingleObjSearch.hpp"
-
-#include "Crossover.hpp"
-#include "DecoderRandomKeys.hpp"
-#include "Elitism.hpp"
-#include "Mutation.hpp"
-#include "Selection.hpp"
-
-// Particle Swarm Optimization - PSO
-
-namespace optframe {
-
 
 template<XEvaluation XEv = Evaluation<>, optframe::comparability KeyType = double>
 
@@ -264,6 +113,22 @@ public:
         }
         return swarm;
     }
+    
+    void BirdMutation ( Bird b)
+    {
+       if (rand01() < 0.1*0.1)
+       {
+          for (unsigned j = 0; j < this->cS.size(); j++ )
+          {
+            double r = rand01();
+            if (r < 0.5)
+               b.position[j] += (cS[j] - cI[j]) * (pow(2.0 *   r   , 1.0 / 21 ) - 1);
+            else
+               b.position[j] += (cS[j] - cI[j]) * (pow(2.0 * (1-r) , 1.0 / 21 ) + 1);
+         
+          }
+       }
+    }
 
     void BoundCheck(Bird& b)    // Rename to assert?
     {
@@ -277,11 +142,11 @@ public:
             b.position[j]= this->cI[j] + (this->cS[i] - this->cI[i]) * rg.rand01();
             b.velocity[j] = 0.1* ( this->cI[j] + (this->cS[i] - this->cI[i]) * rg.rand01() );
             }
-            break
+            break;
         }
         count++;
-    }
-    }
+    } // while
+    } // BoundCheck
 
    virtual ~PSO()
    {
@@ -307,35 +172,52 @@ public:
         Bird& b = swarm[i]
         for (unsigned j = 0; j < this->cS.size(); j++)
         {
-            b.position[j] += b.velocity[j]
+            b.position[j] += b.velocity[j];
         }
-        BoundCheck()
+        BoundCheck();
       }
+      
+      //first global best
+      for (unsigned i = 0; i<this->pop_size; i++)
+      {
+         if (evaluator(swarm[i].localbest) < evaluator(Global)))
+            Global = swarm[i];
+      } 
       
        // count generations
       int count_gen = 0;
       
       while (count_gen < iter_max)
-      {
-
-      // Global Best Maintenance
-      for (unsigned i = 0; i<this->pop_size; i++)
-      {
-         if (evaluator(swarm[i].localbest) < evaluator(Global)))
-            Global = swarm[i];
-      }
+      {      
 
       // Particle update
       for (unsigned i = 0; i<this->pop_size; i++)
       {
          Bird& b = swarm[i]
-         for (unsigned j = 0; j<this->cS.size(); j++)
+         
+         // 0.1 chance of generating a random guy
+         if( rand01() < 0.1)  
          {
-            b.position[j] += b.velocity[j]
+            for (unsigned j = 0; j < this->cS.size(); j++)
+            {
+            b.position[j]= this->cI[j] + (this->cS[i] - this->cI[i]) * rg.rand01();
+            b.velocity[j] = 0.1* ( this->cI[j] + (this->cS[i] - this->cI[i]) * rg.rand01() );
+            }
          }
-         BoundCheck(b)
+
+         // 0.9 chance to just update the particle
+         else
+         {         
+         for (unsigned j = 0; j<this->cS.size(); j++)
+            b.position[j] += b.velocity[j];
+         }
+         
+         BirdMutation(b) // 0.01 chance to do specific mutation
+         
+         // After updating, check to see if there's local upgrades
+         BoundCheck(b);
          if (evaluator(b.position) < evaluator(b.localbest)))
-            b.localbest = b.position
+            b.localbest = b.position;
       }
       
       // Speed update
@@ -349,6 +231,13 @@ public:
          }
       }
 
+      // Global Best Maintenance
+      for (unsigned i = 0; i<this->pop_size; i++)
+      {
+         if (evaluator(swarm[i].localbest) < evaluator(Global)))
+            Global = swarm[i];
+      }
+      
       count_gen ++;
       }  //while count_gen
       
