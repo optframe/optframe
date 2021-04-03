01a571ec64b5c4cb1c66198b0291f5011d25905f
Igor Machado <igor.machado@gmail.com>
2021-04-03 16:09:35 -0300

    clean only directories

--- Examples/clean_all.sh
+++ Examples/clean_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 for example in `ls -I makefile | grep -v "sh$"`; do
-   echo ""
-   echo "Clean Example $example"
-   echo "cd ../Examples/${example} && make clean"
-   (cd ../Examples/${example} && make clean)
-   echo "cd ../Examples/${example} && bazel clean"
-   (cd ../Examples/${example} && bazel clean)
+   # check if it's a directory (ignore local files)
+   if [ -d "../Examples/${example}" ]; then
+      echo ""
+      echo "Clean Example $example"
+      echo "cd ../Examples/${example} && make clean"
+      (cd ../Examples/${example} && make clean)
+      echo "cd ../Examples/${example} && bazel clean"
+      (cd ../Examples/${example} && bazel clean)
+   fi
 done
 
