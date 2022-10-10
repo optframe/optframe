2cecf6be4ce614301a714ef3c42bc8c532d29bb8
Igor Machado <igor.machado@gmail.com>
2022-10-09 21:44:53 -0300

    bump version 5.0.3 -> 5.0.4

--- include/OptFCore/FCore.hpp
+++ include/OptFCore/FCore.hpp
@@ -25,7 +25,7 @@ namespace optframe {
 // OptFrame Functional Core (FCore)
 class FCore {
  public:
-  static constexpr char OPTFRAME_VERSION[] = "5.0.3";
+  static constexpr char OPTFRAME_VERSION[] = "5.0.4";
   static std::string version() {
     return OPTFRAME_VERSION;
   }
