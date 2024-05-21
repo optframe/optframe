32b7beae353a270e25ba0e086d61c1f619695a84
Igor Machado <igor.machado@gmail.com>
2024-05-21 13:33:33 +0000

    add cmake Find module

---
+++ cmake/OptFrameConfig.cmake
@@ -0,0 +1,11 @@
+# avoid reloading
+if(NOT TARGET OptFrame::OptFrameAll)
+    # provide path for scripts (if necessary)
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+    # load top-level cmakelists (not recommended, but easy!)
+    include("${CMAKE_CURRENT_LIST_DIR}/../CMakeLists.txt")
+    # exports include dir
+    set(OptFrame_FOUND ON)
+    set(OptFrame_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../include/")
+    # set(OptFrame_LIBRARIES "")
+endif()
