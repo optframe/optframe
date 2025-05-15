8288d216dc340be8f5859bb451063b57665e0f21
Igor Machado <igor.machado@gmail.com>
2025-05-14 23:47:30 -0300

    unit testing core tsp example

---
+++ include/OptFrame-Core-Examples/FCore-TSP/TSP-fcore.hpp
@@ -0,0 +1,218 @@
+// SPDX-License-Identifier:  MIT OR LGPL-3.0-or-later
+// Copyright (C) 2007-2025 - OptFrame developers
+// https://github.com/optframe/optframe
+
+#ifndef OPTFRAME_CORE_EXAMPLES_FCORE_TSP_TSP_FCORE_HPP_  // NOLINT
+#define OPTFRAME_CORE_EXAMPLES_FCORE_TSP_TSP_FCORE_HPP_  // NOLINT
+
+#if (__cplusplus < 202302L) || !defined(OPTFRAME_CXX_MODULES)
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <utility>
+#include <vector>
+//
+#include <OptFCore/FCoreAll.hpp>
+#include <OptFrame/Concepts/Domain.hpp>
+#include <OptFrame/Core.hpp>
+#include <OptFrame/Heuristics.hpp>  // many metaheuristics here...
+#include <OptFrame/Heuristics/EA/RK/InitialEPopulationRK.hpp>
+#include <OptFrame/Pareto/MultiEvaluator.hpp>
+#include <OptFrame/Pareto/Pareto.hpp>
+#include <OptFrame/Scanner++/Scanner.hpp>
+#include <OptFrame/Search/InitialPopulation.hpp>
+#include <OptFrame/Util/Matrix.hpp>
+#include <OptFrame/printable/printable.hpp>
+
+#define MOD_EXPORT
+#else
+
+// CANNOT IMPORT HERE... Already part of optframe.core
+/*
+import std;
+import optframe.component;
+import optframe.concepts;
+*/
+
+// do NOT import/export modules on .hpp... only on .cppm
+
+#define MOD_EXPORT export
+
+#endif
+
+using namespace std;
+using namespace optframe;
+using namespace scannerpp;
+
+// next definitions come here within namespace
+// this also works when defining in global scope (same as 'class')
+MOD_EXPORT namespace TSP_fcore {
+  using ESolutionTSP = std::pair<std::vector<int>, Evaluation<int>>;
+  // using MINIMIZE = MinOrMax::MINIMIZE;
+
+#if defined(__cpp_concepts) && (__cpp_concepts >= 201907L)
+  static_assert(XESolution<ESolutionTSP>);
+#endif
+
+  // TSP problem context and data reads
+  class ProblemContext {
+   public:
+    sref<RandGen> rg{new RandGen{}};
+    int n;             // number of clients
+    Matrix<int> dist;  // distance matrix (Euclidean)
+    // load data from Scanner
+    void load(Scanner& scanner) {
+      n = *scanner.nextInt();    // reads number of clients
+      dist = Matrix<int>(n, n);  // initializes n x n matrix
+      //
+      vector<double> xvalues(n);
+      vector<double> yvalues(n);
+      //
+      for (int i = 0; i < n; i++) {
+        scanner.next();
+        xvalues[i] = *scanner.nextDouble();  // reads x
+        yvalues[i] = *scanner.nextDouble();  // reads y
+      }
+      // calculate distance values, for every client pair (i,j)
+      for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+          dist(i, j) = ::round(distance(xvalues.at(i), yvalues.at(i),
+                                        xvalues.at(j), yvalues.at(j)));
+    }
+    // euclidean distance (double as return)
+    double distance(double x1, double y1, double x2, double y2) {
+      return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+  };
+  // Create TSP Problem Context
+  // ProblemContextTSP pTSP;
+
+  Evaluation<int> fevaluate(sref<ProblemContext> pTSP,
+                            const std::vector<int>& s) {
+    int f1 = 0;
+    for (int i = 0; i < int(pTSP->n) - 1; i++) f1 += pTSP->dist(s[i], s[i + 1]);
+    f1 += pTSP->dist(s[int(pTSP->n) - 1], s[0]);
+    return Evaluation<int>{f1};
+  }
+
+  std::vector<int> frandom(sref<ProblemContext> pTSP) {
+    vector<int> v(pTSP->n, -1);  // get information from context
+    for (auto i = 0; i < (int)v.size(); i++) v[i] = i;
+    // leave 0 on first position and shuffle the rest
+    for (auto i = 1; i < (int)v.size() - 1; i++) {
+      int j = pTSP->rg->rand(i, v.size() - 1);
+      // swap i and j
+      int aux = v[i];
+      v[i] = v[j];
+      v[j] = aux;
+    }
+    // std::random_device rd;
+    // std::mt19937 g(rd());
+    // std::shuffle(v.begin() + 1, v.end(), g);
+    return v;
+  }
+
+  //
+  class MoveSwap : public Move<ESolutionTSP> {
+   public:
+    int i, j;
+
+    MoveSwap(int _i, int _j) : i{_i}, j{_j} {}
+
+    bool canBeApplied(const ESolutionTSP& se) override {
+      return (::abs(i - j) >= 2) && (i >= 1) && (j >= 1);
+    }
+
+    uptr<Move<ESolutionTSP>> apply(ESolutionTSP& se) override {
+      // perform swap of clients i and j
+      int aux = se.first[j];
+      se.first[j] = se.first[i];
+      se.first[i] = aux;
+      return uptr<Move<ESolutionTSP>>(new MoveSwap{j, i});
+    }
+
+    bool operator==(const Move<ESolutionTSP>& other) const override {
+      auto& fmove = (MoveSwap&)other;
+      return (i == fmove.i) && (j == fmove.j);
+    }
+
+    std::string toString() const override {
+      std::stringstream ss;
+      ss << "MoveSwap(i=" << i << "; j=" << j << ")";
+      return ss.str();
+    }
+  };
+
+  uptr<Move<ESolutionTSP>> makeMoveSwap(sref<ProblemContext> pTSP, int i,
+                                        int j) {
+    return uptr<Move<ESolutionTSP>>(new MoveSwap{i, j});
+  }
+
+  uptr<Move<ESolutionTSP>> fRandomSwap(sref<ProblemContext> pTSP,
+                                       const ESolutionTSP& se) {
+    int i = pTSP->rg->rand() % pTSP->n;
+    int j = i;
+    while ((j <= i) || (i == 0)) {
+      i = pTSP->rg->rand() % pTSP->n;
+      j = pTSP->rg->rand() % pTSP->n;
+    }
+
+    return uptr<Move<ESolutionTSP>>(makeMoveSwap(pTSP, i, j));
+  }
+
+  class OptFrameCoreTSP {
+   public:
+    using S = typename ESolutionTSP::first_type;
+    using XEv = typename ESolutionTSP::second_type;
+    using XES = ESolutionTSP;
+    FConstructive<std::vector<int>, ProblemContext> crand;
+    FEvaluator<XES, MINIMIZE, ProblemContext> ev;
+    FNS<ESolutionTSP, ProblemContext> nsswap;
+    FNSSeq<std::pair<int, int>, ESolutionTSP, ProblemContext> nsseq;
+
+    explicit OptFrameCoreTSP(sref<ProblemContext> p)
+        : crand{p, frandom},
+          ev{p, fevaluate},
+          nsswap{p, fRandomSwap},
+          nsseq{p,
+                [](sref<ProblemContext> pTSP,
+                   const ESolutionTSP& se) -> uptr<Move<ESolutionTSP>> {
+                  int i = rand() % pTSP->n;
+                  int j = i;
+                  while (j <= i) {
+                    i = rand() % pTSP->n;
+                    j = rand() % pTSP->n;
+                  }
+                  return uptr<Move<ESolutionTSP>>(makeMoveSwap(pTSP, i, j));
+                },
+
+                [](sref<ProblemContext> pTSP, const ESolutionTSP& se)
+                    -> std::pair<int, int> { return make_pair(-1, -1); },
+                [](sref<ProblemContext> pTSP, std::pair<int, int>& p) -> void {
+                  p.first = 0;
+                  p.second = 1;
+                },
+                [](sref<ProblemContext> pTSP, std::pair<int, int>& p) -> void {
+                  if (p.second < (pTSP->n - 1)) {
+                    p.second++;
+                  } else {
+                    p.first++;
+                    p.second = p.first + 1;
+                  }
+                },
+                [](sref<ProblemContext> pTSP, std::pair<int, int>& p) -> bool {
+                  return p.first >= pTSP->n - 1;
+                },
+                [](sref<ProblemContext> pTSP,
+                   std::pair<int, int>& p) -> uptr<Move<ESolutionTSP>> {
+                  return uptr<Move<ESolutionTSP>>(
+                      makeMoveSwap(pTSP, p.first, p.second));
+                }} {
+      //
+    }
+  };
+
+}  // namespace BTSP_fcore
+
+#endif  // OPTFRAME_CORE_EXAMPLES_FCORE_TSP_TSP_FCORE_HPP_ // NOLINT
---
+++ tests/OptFrameLib-test/optframelib_tsp.Test.cpp
@@ -0,0 +1,246 @@
+
+#include <boost/ut.hpp>
+
+// OptFrame C library headers
+#include <OptFrameLib/OptFrameLib.h>
+
+#include <OptFrame-Core-Examples/FCore-TSP/TSP-fcore.hpp>
+#include <OptFrame/modlog/modlog.hpp>
+#include <OptFrameLib/FCoreApi1Engine.hpp>
+
+#include "OptFrameLib/LibCTypes.h"
+
+using TSP_fcore::ESolutionTSP;
+using TSP_fcore::MoveSwap;
+
+// directly testing optframe C library (static)
+
+double fevaluate_c(FakeProblemPtr p_ptr, FakeSolutionPtr s_ptr) {
+  using PTSP = TSP_fcore::ProblemContext;
+  std::shared_ptr<PTSP> problem_view{(PTSP*)p_ptr, [](PTSP*) {}};
+  sref<PTSP> problem{problem_view};
+  auto* v = (std::vector<int>*)(s_ptr);
+  auto e = TSP_fcore::fevaluate(problem, *v);
+  return e.evaluation();
+}
+
+FakeSolutionPtr fconstructive_c(FakeProblemPtr p_ptr) {
+  using PTSP = TSP_fcore::ProblemContext;
+  std::shared_ptr<PTSP> problem_view{(PTSP*)p_ptr, [](PTSP*) {}};
+  sref<PTSP> problem{problem_view};
+  auto v = TSP_fcore::frandom(problem);
+  return new std::vector<int>(std::move(v));
+}
+
+FakeMovePtr fnsrand_c(FakeProblemPtr p_ptr, FakeSolutionPtr s_ptr) {
+  using PTSP = TSP_fcore::ProblemContext;
+  std::shared_ptr<PTSP> problem_view{(PTSP*)p_ptr, [](PTSP*) {}};
+  sref<PTSP> problem{problem_view};
+  auto* v = (std::vector<int>*)s_ptr;
+  auto m =
+      TSP_fcore::fRandomSwap(problem, ESolutionTSP{*v, Evaluation<int>{0}});
+  auto* m_ptr = m.release();
+  return m_ptr;
+}
+
+FakeMovePtr fmove_apply_c(FakeProblemPtr p_ptr, FakeMovePtr m_ptr,
+                          FakeSolutionPtr s_ptr) {
+  auto* m = (MoveSwap*)m_ptr;
+  int i = m->i;
+  int j = m->j;
+  auto* v = (std::vector<int>*)s_ptr;
+  int aux = (*v)[j];
+  (*v)[j] = (*v)[i];
+  (*v)[i] = aux;
+  return new MoveSwap{j, i};
+}
+
+bool fmove_eq_c(FakeProblemPtr p_ptr, FakeMovePtr m1_ptr, FakeMovePtr m2_ptr) {
+  auto* m1 = (MoveSwap*)m1_ptr;
+  auto* m2 = (MoveSwap*)m2_ptr;
+  return (m1->i == m2->i) && (m1->j == m2->j);
+}
+
+bool fmove_cba_c(FakeProblemPtr p_ptr, FakeMovePtr m_ptr,
+                 FakeSolutionPtr s_ptr) {
+  return true;
+}
+
+FakePythonObjPtr fIterator_c(FakeProblemPtr, FakeSolutionPtr) {
+  return new MoveSwap{0, 0};
+}
+void fFirst_c(FakeProblemPtr, FakePythonObjPtr it_ptr) {
+  auto* m = (MoveSwap*)it_ptr;
+  m->i = 0;
+  m->j = 1;
+}
+void fNext_c(FakeProblemPtr p_ptr, FakePythonObjPtr it_ptr) {
+  auto* problem = (TSP_fcore::ProblemContext*)p_ptr;
+  auto* m = (MoveSwap*)it_ptr;
+  if (m->j < (problem->n - 1)) {
+    m->j++;
+  } else {
+    m->i++;
+    m->j = m->i + 1;
+  }
+}
+bool fIsDone_c(FakeProblemPtr p_ptr, FakePythonObjPtr it_ptr) {
+  auto* problem = (TSP_fcore::ProblemContext*)p_ptr;
+  auto* m = (MoveSwap*)it_ptr;
+  return m->i >= problem->n - 1;
+}
+FakeMovePtr fCurrent_c(FakeProblemPtr p_ptr, FakePythonObjPtr it_ptr) {
+  auto* m = (MoveSwap*)it_ptr;
+  return new MoveSwap{m->i, m->j};
+}
+
+FakeSolutionPtr f_sol_deepcopy(FakeSolutionPtr s_ptr) {
+  auto* v = (std::vector<int>*)(s_ptr);
+  return new std::vector<int>(*v);
+}
+
+size_t f_sol_tostring(FakeSolutionPtr s_ptr, char* str, size_t str_size) {
+  auto* v = (std::vector<int>*)(s_ptr);
+  std::stringstream ss;
+  ss << *v;
+  std::string s = ss.str();
+  strcpy(str, s.c_str());
+  return s.length();
+}
+
+int f_decref_solution(FakeSolutionPtr s_ptr) {
+  auto* v = (std::vector<int>*)(s_ptr);
+  delete v;
+  return true;
+}
+
+int f_decref_move(FakeMovePtr m_ptr) {
+  auto* m = (Move<ESolutionTSP>*)(m_ptr);
+  delete m;
+  return true;
+}
+
+int main() {
+  using namespace boost::ut;
+
+  FakeEnginePtr engine =
+      optframe_api1d_create_engine((int)modlog::LogLevel::Disabled);
+
+  std::stringstream ss;
+  auto* eng = (FCoreApi1Engine*)engine;
+  eng->logstream = &ss;
+  eng->engineLogLevel = modlog::LogLevel::Debug;
+
+  optframe_api0d_engine_welcome(engine);
+
+  std::string welcome_message = ss.str();
+  ss.clear();
+
+  "welcome"_test = [welcome_message] {
+    expect((welcome_message.find(
+                "Welcome to OptFrame Functional Core (FCore) - version") !=
+            std::string::npos));
+  };
+
+  // testing welcome again on std::cout
+  eng->logstream = &std::cout;
+  optframe_api0d_engine_welcome(engine);
+
+  "first_rand"_test = [eng] {
+    eng->loader.factory.getRandGen()->setSeed(9999);
+    expect(eng->loader.factory.getRandGen()->rand() % 100 == 5_i);
+  };
+
+  "optframe_api1d_engine_rand"_test = [engine] {
+    optframe_api1d_engine_rand_set_seed(engine, 9999);
+    expect(optframe_api1d_engine_rand(engine) % 100 == 5_i);
+  };
+
+  // manipulating seed from rand in engine
+  eng->loader.factory.getRandGen()->setSeed(0);
+
+  sref<TSP_fcore::ProblemContext> problem{new TSP_fcore::ProblemContext{}};
+  Scanner scanner{"5\n1 1.0 1.0\n2 2.0 5.0\n3 3.0 4.0\n4 4.0 3.0\n5 5.0 2.0"};
+  problem->load(scanner);
+  problem->rg = eng->loader.factory.getRandGen();
+  // get problem pointer as void*
+  FakeProblemPtr p_ptr = (void*)(&problem.get());
+
+  // testing initial solutions
+
+  auto sol = TSP_fcore::frandom(problem);
+  // sol = vector(5) [0 , 4 , 2 , 3 , 1]
+
+  "sol"_test = [sol] {
+    expect(sol[0] == 0_i);
+    expect(sol[1] == 4_i);
+    expect(sol[2] == 2_i);
+    expect(sol[3] == 3_i);
+    expect(sol[4] == 1_i);
+  };
+
+  auto e = TSP_fcore::fevaluate(problem, sol);
+
+  "e"_test = [e] { expect(e.evaluation() == 15_i); };
+
+  auto esol = ESolutionTSP{sol, e};
+
+  int idx_ev = optframe_api1d_add_evaluator(engine, fevaluate_c, false, p_ptr);
+
+  "optframe_api1d_add_evaluator"_test = [idx_ev] { expect(idx_ev == 0_i); };
+
+  int idx_c = optframe_api1d_add_constructive(engine, fconstructive_c, p_ptr,
+                                              f_sol_deepcopy, f_sol_tostring,
+                                              f_decref_solution);
+
+  "optframe_api1d_add_constructive"_test = [idx_c] { expect(idx_c == 0_i); };
+
+  int idx_is = optframe_api1d_create_initial_search(engine, idx_ev, idx_c);
+
+  "optframe_api1d_create_initial_search"_test = [idx_is] {
+    expect(idx_is == 0_i);
+  };
+
+  // problem->rg->setSeed(1);
+
+  "random_move"_test = [problem, esol] {
+    auto m = TSP_fcore::fRandomSwap(problem, esol);
+    expect(m->toString() == std::string("MoveSwap(i=2; j=4)"));
+    expect(m->canBeApplied(esol));
+  };
+
+  int idx_ns =
+      optframe_api1d_add_ns(engine, fnsrand_c, fmove_apply_c, fmove_eq_c,
+                            fmove_cba_c, p_ptr, f_decref_move);
+
+  "optframe_api1d_add_ns"_test = [idx_ns] { expect(idx_ns == 0_i); };
+
+  int idx_nsseq = optframe_api1d_add_nsseq(
+      engine, fnsrand_c, fIterator_c, fFirst_c, fNext_c, fIsDone_c, fCurrent_c,
+      fmove_apply_c, fmove_eq_c, fmove_cba_c, p_ptr, f_decref_move);
+
+  "optframe_api1d_add_nsseq"_test = [idx_nsseq] { expect(idx_nsseq == 0_i); };
+
+  // =====================
+
+  // test check module
+
+  bool expr = optframe_api1d_engine_check(
+      engine, 10, 5, false, [](int, FakeEnginePtr eng) -> bool {
+        // This enables debugging mode, if component supports it...
+        // TODO: create an example where check fails and we verify the logs
+        auto* engine = (FCoreApi1Engine*)eng;
+        engine->experimentalParams["COMPONENT_LOG_LEVEL"] = "-1";
+        engine->updateParameters();
+        return true;
+      });
+  "optframe_api1d_engine_check"_test = [expr] { expect(expr); };
+
+  // =====================
+
+  bool b = optframe_api1d_destroy_engine(engine);
+
+  "optframe_api1d_destroy_engine"_test = [b] { expect(b == 1_i); };
+
+  return 0;
+}
